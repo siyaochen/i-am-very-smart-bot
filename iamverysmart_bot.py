b690866ec7f83bb6525eefd9f617917e6bfc455b
siyaochen <siyaochen416@gmail.com>
2017-03-23 08:27:55 -0400

    Added login and access code

---
+++ iamverysmart_bot.py
@@ -0,0 +1,62 @@
+import praw
+import time
+import PyDictionary
+
+excluded_words = ["I", "that", "the", "they", ""]
+# Append to cache if performed action (write to .txt file?)
+cache = []
+
+# Get password
+print("Getting password...")
+password_file = open("bot_password.txt")
+password = password_file.read()
+print("Password successfully read.")
+
+# Login
+print("Attempting to login...")
+r = praw.Reddit(user_agent = "I-am-very-smart bot 1.0 /u/iamverysmart_bot")
+r.login("iamverysmart_bot", password)
+print("Successfully logged in.")
+
+# Given a word, returns the longest synonym
+def longest_synonym(word):
+	dictionary = PyDictionary()
+	synonyms = dictionary.synonym(word)
+	longest = ""
+
+	max_length = 0
+	# If synonyms are available
+	if synonyms is not None:
+		for word in synonyms:
+			if len(word) > max_length:
+				max_length = len(word)
+				longest = word
+	
+	return longest
+
+# Replaces a word within a comment with another word
+def replace(comment, word):
+	#Stuff here
+
+# Run the bot
+def run():
+	subreddit = r.get_subreddit("test")
+	comments = subreddit.get_comments(limit = 10)
+
+	# Loop through comments, reply to some
+	for comment in comments:
+		if len(comment) > 30 and len(comment) < 200:
+			new_comment = comment
+			text = comment.body
+			for word in text:
+				# If the word matches the criteria
+				if not (word[0].isupper() and word in excluded_words) and len(word) >= 5:
+					new_word = longest_synonym(word)
+				# Replace word with synonym
+				new_commment = replace(new_comment, new_word)
+
+while True:
+	run()
+	# Sleep for 10 seconds
+	time.sleep(10)
+
