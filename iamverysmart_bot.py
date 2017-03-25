79aeb7954dd418e6df54d413739b2e5d3a062f25
siyaochen <siyaochen416@gmail.com>
2017-03-24 21:57:20 -0400

    Added basic exclude list and updated formatting

--- iamverysmart_bot.py
+++ iamverysmart_bot.py
@@ -2,9 +2,12 @@ import praw
 import time
 import PyDictionary
 
-excluded_words = ["I", "that", "the", "they", ""]
-# Append to cache if already processed (write to .txt file?)
+# Words that aren't replaced
+excluded_words = ["your", "I", "they", "their", "we", "who", "them", "its", "our", "my", "those", "he", "us", "her", "something", "me", "yourself", "someone", "everything", "itself", "everyone", "themselves", "anyone", "him", "whose", "myself", "everybody", "ourselves", "himself", "somebody", "yours", "herself", "whoever", "you", "that", "it", "this", "what", "which", "these", "his", "she", "lot", "anything", "whatever", "nobody", "none", "mine", "anybody", "some", "there", "all", "where", "another", "same", "certain", "nothing", "self", "nowhere"]
+# Append comment to cache if already processed (write to .txt file?)
 cache = []
+# Append word to word_cache if already replaced
+word_cache = []
 
 # Login
 print("Attempting to login...")
@@ -39,16 +42,19 @@ def run():
 
 	# Loop through comments, reply to some
 	for comment in comments:
+		# Check to see if comment length matches
 		if len(comment) > 30 and len(comment) < 200:
 			new_comment = comment
 			text = comment.body
 			for word in text:
-				# If the word matches the criteria
-				if not (word[0].isupper() and word in excluded_words) and len(word) >= 5:
+				new_word = ""
+				# If the word matches all the criteria
+				if not word[0].isupper() and not word in excluded_words and len(word) >= 5:
 					new_word = longest_synonym(word)
-				# Replace word with synonym
-				new_commment = replace(new_comment, new_word)
+					# Replace word with synonym
+					new_commment = replace(new_comment, new_word)
 
+# Main loop
 while True:
 	run()
 	# Sleep for 10 seconds
