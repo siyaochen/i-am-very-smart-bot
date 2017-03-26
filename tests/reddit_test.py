3897bea49a49409faf73e2bf89710b5d4a992603
siyaochen <siyaochen416@gmail.com>
2017-03-26 12:48:50 -0400

    Updated reddit_test with commenting functionality
    Only can post 1 comment every 9 minutes due to low reddit karma.

--- tests/reddit_test.py
+++ tests/reddit_test.py
@@ -2,16 +2,19 @@ import praw
 import time
 
 print("Logging in...")
-reddit = praw.Reddit(client_id='tQ3AcC5Kro-9Rw', client_secret="lcVvrLqY9PLGJRIKa0bLxml-Z1k", user_agent='Test Bot by /u/iamverysmart_bot')
+reddit = praw.Reddit(client_id = "tQ3AcC5Kro-9Rw", client_secret = "lcVvrLqY9PLGJRIKa0bLxml-Z1k", user_agent = "test_bot by /u/iamverysmart_bot", username = "iamverysmart_bot", password = "iamabotbleepbloop")
+print(reddit.user.me())
 print("Login successful.")
 
 def run():
 	print("Contacting subreddit...")
-	subreddit = reddit.subreddit("learnprogramming")
+	subreddit = reddit.subreddit("test")
 	print("Getting comments...")
-	for submission in subreddit.hot(limit=10):
+	for submission in subreddit.hot(limit=2):
 		comments = submission.comments
 		for comment in comments:
-			print(comment.body)
+			comment.reply("This is a test")
+			print("Comment successful. Sleeping for 10 seconds...")
+			time.sleep(10)
 
 run()
